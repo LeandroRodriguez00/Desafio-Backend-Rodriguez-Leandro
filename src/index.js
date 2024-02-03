868719c58f24bbc0c552d9f10f0c59c56b25204a
Leandro Rodriguez <leandrorodriguez1130@gmail.com>
2024-02-03 19:01:54 -0300

    Websockets + Handlebars

--- src/index.js
+++ src/index.js
@@ -1,116 +1,132 @@
 const express = require('express');
-const fs = require('fs');
+const exphbs = require('express-handlebars');
+const { readFile, writeFile } = require('fs').promises;
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const PORT = 8080;
 
+const hbs = exphbs.create();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-const productosRouter = express.Router();
-const productosFilePath = path.join(__dirname, 'productos.json');
+const dataFilePath = {
+    productos: path.join(__dirname, 'productos.json'),
+    carritos: path.join(__dirname, 'carrito.json')
+};
 
-const readProductosFile = () => {
+const readDataFile = async (dataType) => {
     try {
-        const productosData = fs.readFileSync(productosFilePath, 'utf8');
-        return JSON.parse(productosData);
+        const data = await readFile(dataFilePath[dataType], 'utf8');
+        return JSON.parse(data);
     } catch (error) {
         return [];
     }
 };
 
-const writeProductosFile = (data) => {
-    fs.writeFileSync(productosFilePath, JSON.stringify(data, null, 2), 'utf8');
+const writeDataFile = async (dataType, data) => {
+    await writeFile(dataFilePath[dataType], JSON.stringify(data, null, 2), 'utf8');
 };
 
-productosRouter.get('/', (req, res) => {
-    const productos = readProductosFile();
+const handleNotFoundError = (res, entityType) => {
+    res.status(404).json({ error: `${entityType} no encontrado o no existe en la base de datos.` });
+};
+
+const productosRouter = express.Router();
+
+productosRouter.get('/', async (req, res) => {
+    const productos = await readDataFile('productos');
     res.json(productos);
 });
 
-productosRouter.get('/:pid', (req, res) => {
-    const productos = readProductosFile();
+productosRouter.get('/:pid', async (req, res) => {
+    const productos = await readDataFile('productos');
     const producto = productos.find((p) => p.id === req.params.pid);
 
     if (producto) {
         res.json(producto);
     } else {
-        res.status(404).json({ error: 'Producto no encontrado' });
+        handleNotFoundError(res, 'Producto');
     }
 });
 
-productosRouter.post('/', (req, res) => {
-    const productos = readProductosFile();
+productosRouter.post('/', async (req, res) => {
+    const productos = await readDataFile('productos');
     const nuevoProducto = {
         id: Date.now().toString(),
         ...req.body,
     };
     productos.push(nuevoProducto);
-    writeProductosFile(productos);
+    await writeDataFile('productos', productos);
+
+    io.emit('updateProducts', productos);
+
     res.json(nuevoProducto);
 });
 
-productosRouter.put('/:pid', (req, res) => {
-    const productos = readProductosFile();
+productosRouter.put('/:pid', async (req, res) => {
+    const productos = await readDataFile('productos');
     const index = productos.findIndex((p) => p.id === req.params.pid);
     if (index !== -1) {
         productos[index] = { ...productos[index], ...req.body, id: req.params.pid };
-        writeProductosFile(productos);
+        await writeDataFile('productos', productos);
+
+        io.emit('updateProducts', productos);
+
         res.json(productos[index]);
     } else {
-        res.status(404).json({ error: 'Producto no encontrado' });
+        handleNotFoundError(res, 'Producto');
     }
 });
 
-productosRouter.delete('/:pid', (req, res) => {
-    const productos = readProductosFile();
+productosRouter.delete('/:pid', async (req, res) => {
+    const productos = await readDataFile('productos');
     const index = productos.findIndex((p) => p.id === req.params.pid);
     if (index !== -1) {
         const deletedProduct = productos.splice(index, 1)[0];
-        writeProductosFile(productos);
+        await writeDataFile('productos', productos);
+
+        io.emit('updateProducts', productos);
+
         res.json(deletedProduct);
     } else {
-        res.status(404).json({ error: 'Producto no encontrado' });
+        handleNotFoundError(res, 'Producto');
     }
 });
 
 app.use('/api/products', productosRouter);
 
 const carritosRouter = express.Router();
-const carritosFilePath = path.join(__dirname, 'carrito.json');
 
-const readCarritosFile = () => {
-    try {
-        const carritosData = fs.readFileSync(carritosFilePath, 'utf8');
-        return JSON.parse(carritosData);
-    } catch (error) {
-        return [];
-    }
-};
-
-const writeCarritosFile = (data) => {
-    fs.writeFileSync(carritosFilePath, JSON.stringify(data, null, 2), 'utf8');
-};
-
-carritosRouter.post('/', (req, res) => {
-    const carritos = readCarritosFile();
+carritosRouter.post('/', async (req, res) => {
+    const carritos = await readDataFile('carritos');
     const nuevoCarrito = {
         id: Date.now().toString(),
         products: [],
     };
     carritos.push(nuevoCarrito);
-    writeCarritosFile(carritos);
+    await writeDataFile('carritos', carritos);
     res.json(nuevoCarrito);
 });
 
-carritosRouter.get('/:cid', (req, res) => {
-    const carritos = readCarritosFile();
+carritosRouter.get('/:cid', async (req, res) => {
+    const carritos = await readDataFile('carritos');
     const carrito = carritos.find((c) => c.id === req.params.cid);
     res.json(carrito ? carrito.products : []);
 });
 
-carritosRouter.post('/:cid/product/:pid', (req, res) => {
-    const carritos = readCarritosFile();
+carritosRouter.post('/:cid/product/:pid', async (req, res) => {
+    const carritos = await readDataFile('carritos');
     const carritoIndex = carritos.findIndex((c) => c.id === req.params.cid);
 
     if (carritoIndex !== -1) {
@@ -124,15 +140,33 @@ carritosRouter.post('/:cid/product/:pid', (req, res) => {
             carrito.products.push({ product: productoId, quantity: 1 });
         }
 
-        writeCarritosFile(carritos);
+        await writeDataFile('carritos', carritos);
         res.json(carrito.products);
     } else {
-        res.status(404).json({ error: 'Carrito no encontrado' });
+        handleNotFoundError(res, 'Carrito');
     }
 });
 
 app.use('/api/carts', carritosRouter);
 
-app.listen(PORT, () => {
+app.get('/', async (req, res) => {
+    const productos = await readDataFile('productos');
+    res.render('layouts/home', { productos });
+});
+
+app.get('/realtimeproducts', async (req, res) => {
+    const productos = await readDataFile('productos');
+    res.render('layouts/realTimeProducts', { productos });
+});
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado');
+
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+});
+
+server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
